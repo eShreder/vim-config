6179f12ec2b83286790d243c243a8e7b79dcfadc
Evgenii Shreder <eshreder@gmail.com>
2023-10-02 09:46:43 +0200

    feat: add colorscheme ayu

--- lua/plugins/colors/init.lua
+++ lua/plugins/colors/init.lua
@@ -1,12 +1,18 @@
-return {
-    "ellisonleao/gruvbox.nvim",
-    lazy = false,
-    config = function()
-        require("gruvbox").setup({
-            transparent_mode = false,
-            italic = {
-            },
-        })
-        vim.cmd("colorscheme gruvbox")
-    end
-}
+return {{
+  "ellisonleao/gruvbox.nvim",
+  lazy = false,
+  config = function()
+    require("gruvbox").setup({
+      transparent_mode = false,
+      italic = {
+      },
+    })
+    vim.cmd("colorscheme gruvbox")
+  end
+}, {
+  "ayu-theme/ayu-vim",
+  lazy = false,
+  config = function()
+    vim.cmd("colorscheme ayu")
+  end
+}}
